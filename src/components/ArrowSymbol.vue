489df7bb78c16d474341bd204b59ec9ad1c605ba
Ben Hong <ben@bencodezen.io>
2024-05-29 11:30:34 -0700

    feat: add arrow symbol component

---
+++ src/components/ArrowSymbol.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    color: string
+    direction: string
+  }>(),
+  {
+    color: 'green',
+    direction: 'up'
+  }
+)
+</script>
+
+<template>
+  <svg
+    :class="`is-${direction}`"
+    width="120"
+    height="120"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M38.5362 33.25V59.5H24.301c-7.829 0-14.2188.0329-14.2023.0657.0329.0984 49.8519 52.4183 49.9013 52.4183.0493 0 49.868-52.3199 49.901-52.4183.017-.0328-6.373-.0657-14.202-.0657H81.4638V7.00004H38.5362V33.25Z"
+      :fill="color"
+    />
+  </svg>
+</template>
+
+<style scoped>
+.is-up {
+  transform: rotate(180deg);
+}
+
+.is-down {
+  transform: rotate(0deg);
+}
+
+.is-left {
+  transform: rotate(90deg);
+}
+
+.is-right {
+  transform: rotate(-90deg);
+}
+</style>
--- src/pages/play/round-[id].vue
+++ src/pages/play/round-[id].vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+
+const validKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
+const sequence = ref([
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight'
+])
 </script>
 
 <template>
   <p>{{ route }}</p>
+  <ul style="display: flex">
+    <li v-for="arrow in sequence">
+      <ArrowSymbol :direction="arrow.substring(5).toLowerCase()" color="#ccc" />
+    </li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
