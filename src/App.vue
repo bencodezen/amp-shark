05871b72f07f389ab88f3e519f0ff987caa16f2a
Ben Hong <ben@bencodezen.io>
2024-05-29 15:13:12 -0700

    feat: add easter egg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { onKeyStroke } from '@vueuse/core'
 
 const route = useRoute()
+const router = useRouter()
+
+const cheatPosition = ref(0)
+const konami = ref([
+  'ArrowUp',
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowLeft',
+  'ArrowRight',
+  'b',
+  'a'
+])
+
+onKeyStroke(true, e => {
+  if (e.key === konami.value[cheatPosition.value]) {
+    cheatPosition.value++
+  }
+
+  if (cheatPosition.value === konami.value.length) {
+    alert('Konami Code Activated!')
+    window.location = 'https://www.youtube.com/watch?v=mx86-rTclzA'
+  }
+})
 </script>
 
 <template>
