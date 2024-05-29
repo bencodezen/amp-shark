14a8c97e79f37f423b730f0662bc8bceebdb496d
Ben Hong <ben@bencodezen.io>
2024-05-29 11:41:42 -0700

    fix: resolve issue with pages not reloading

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,6 @@ import { createWebHistory, createRouter } from 'vue-router'
 import routes from './routes'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
